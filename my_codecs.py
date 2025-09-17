4f80873ef1c79b103d3d22c0853bf262f0f9bc9c
John Doe <kamkilljoy@gmail.com>
2025-09-17 15:54:55 +0400

    изменено хранение допустимых опций

--- my_codecs.py
+++ my_codecs.py
@@ -7,7 +7,6 @@ class Codec:
             even_res: bool = True,
             vcodec: str = None,
             acodec: str = None,
-            extra_opts: str = ""
     ):
         self.name = name
         self.params = params
@@ -26,7 +25,6 @@ class Codec:
         return f"<Codec {self.name}>"
 
 
-
 # -------------------
 # VP9
 # -------------------
@@ -34,12 +32,12 @@ class Codec:
 vp9 = Codec(
     name="vp9",
     params={
-        "crf": range(0, 64),  # 0–63
+        "crf": [0, 63],
         "scale": None,  # свободный ввод
         "fps": None,  # свободный ввод
         "preset": ["realtime", "good", "best"],
         "pixel_format": ["yuv420p", "yuv422p", "yuv444p"],
-        "passes": [1, 2],  # One-Pass / Two-Pass
+        "passes": ["One-Pass", "Two-Pass"],
         "container": ["webm", "mkv"]
     },
     even_res=True,
@@ -53,12 +51,12 @@ vp9 = Codec(
 svt_av1 = Codec(
     name="svt-av1",
     params={
-        "crf": range(0, 64),
+        "crf": [0, 63],
         "scale": None,
         "fps": None,
         "preset": list(range(0, 10)),  # 0–9
         "pixel_format": ["yuv420p", "yuv422p", "yuv444p"],
-        "passes": [1, 2],
+        "passes": ["One-Pass", "Two-Pass"],
         "container": ["mkv", "mp4", "webm"]
     },
     even_res=False,
@@ -72,13 +70,13 @@ svt_av1 = Codec(
 hevc265 = Codec(
     name="hevc",
     params={
-        "crf": range(0, 52),  # libx265 обычно 0–51
+        "crf": [0, 51],
         "scale": None,
         "fps": None,
         "preset": ["ultrafast", "superfast", "veryfast", "faster", "fast",
                    "medium", "slow", "slower", "veryslow", "placebo"],
         "pixel_format": ["yuv420p", "yuv422p", "yuv444p"],
-        "passes": [1, 2],
+        "passes": ["One-Pass", "Two-Pass"],
         "container": ["mp4", "mkv", "mov", "ts"]
     },
     even_res=True,
