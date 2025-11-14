49ec24b44120b5161dfd475a06465f50114b1259
John Doe <kamkilljoy@gmail.com>
2025-11-14 14:06:29 +0400

    починил составление подстроки опций

--- internal/commander.py
+++ internal/commander.py
@@ -21,9 +21,28 @@ class Commander:
     def __build_options_string(self,
                                context: str,
                                option_flag: str,
-                               separator: str,
+                               kv_separator: str,
+                               join_sep: str,
                                exclude: tuple[str, ...] = ()
                                ) -> str:
+        """
+        Формирует строку опций ffmpeg для конкретного контекста, готовую для вставки в команду.
+
+        Аргументы:
+            context (str): Контекст настроек для обработки
+                (например, "video filters", "audio filters" или "global").
+            option_flag (str): Флаг, который будет добавлен перед опциями
+                (например, "-vf" для видеофильтров). Можно оставить пустым для глобальных опций.
+            kv_separator (str): Разделитель между флагом и его значением
+                (например, "=" для "-crf=21" или пробел для "-crf 21").
+            join_sep (str): Разделитель между несколькими опциями в результирующей строке
+                (например, "," для фильтров или пробел для глобальных опций).
+            exclude (tuple[str, ...], optional): Ключи настроек, которые нужно исключить из результата. По умолчанию ().
+
+        Возвращает:
+            str: Сформированная часть строки командной строки с опциями для ffmpeg.
+                 Если нет валидных опций — возвращается пустая строка.
+        """
 
         o_params = self.__parse_params(context)
         params_list = []
@@ -36,9 +55,9 @@ class Commander:
             flag = value.get("flag")
             if not flag:
                 continue
-            params_list.append(f"{flag}{separator}{val}")
+            params_list.append(f"{flag}{kv_separator}{val}")
 
-        o_string = f'{option_flag} {" ".join(params_list)}' if params_list else ""
+        o_string = f'{option_flag} {f"{join_sep}".join(params_list)}' if params_list else ""
         return o_string
 
     def build_ffmpeg_command(self, file: str, output_dir) -> str:
@@ -52,9 +71,9 @@ class Commander:
         passes = self.settings.get("passes")
         container = self.settings.get("container")
 
-        video_filters = self.__build_options_string("video filters", "-vf", "=")
-        audio_filters = self.__build_options_string("audio filters", "-af", "=")
-        global_options = self.__build_options_string("global", "", " ")
+        video_filters = self.__build_options_string("video filters", "-vf", "=", ", ")
+        audio_filters = self.__build_options_string("audio filters", "-af", "=", ", ")
+        global_options = self.__build_options_string("global", "", " ", " ")
 
         output_file = os.path.join(output_dir, f'{filename}_{self.codec.name}{param_for_name}')
 
